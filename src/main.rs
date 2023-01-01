07f02cb2424c19fad8d75a2e530364c144390ff4
nick <gitcommit.email@gmail.com>
2023-01-01 15:36:28 +1100

    more currency added!

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use std::io::stdin;
-
 fn main() {
-    // maim 
+    // asks for options!
     let mut choice = String::new();
-    println!("Convert [aud] to [usd] [gbp] [cad], choices:\n usd\n gbp\n cad \n");
+    println!("Convert [aud] to [usd] [gbp] [cad] [nzd] [zar], choices:\n usd\n gbp\n cad \n nzd \n zar\n");
     std::io::stdin().read_line(&mut choice).expect("failed to read line");
 
+
+    // options for currency:
     let currency = choice.trim();
     match currency {
         "usd" => convert("usd", 0.681359),
         "gbp" => convert("gbp", 0.5637),
         "cad" => convert("cad", 0.92249222),
+        "nzd" => convert("nzd", 0.9330),
+        "zar" => convert("zar", 0.0863),
         _ => println!("Invalid choice"),
     }
 }
-
+// convert function!
 fn convert(currency: &str, rate: f64) {
     let mut aud = String::new();
     std::io::stdin().read_line(&mut aud).expect("cannot read line");
