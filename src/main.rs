14fef8d5bb19286fd8a91f2865a82a4212a27520
phant80m <black.pandazayd@gmail.com>
2023-06-04 20:30:07 +1000

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Jcurrency {
 
 #[derive(Debug, Parser)]
 struct Args {
-    #[arg(long, short)]
+    #[arg(short, long)]
     input: String,
     #[arg(short, long)]
     output: String,
@@ -22,34 +22,59 @@ fn main() {
     let arguments = Args::parse();
     let _cconvert = convert(arguments.input.clone());
 
-    let output = deserialize(_cconvert, arguments.output.to_uppercase(), arguments.amount);
+    let output = match deserialize(
+        _cconvert.unwrap_or_default(),
+        arguments.output.to_uppercase(),
+        arguments.amount,
+    ) {
+        Ok(value) => value,
+        Err(err) => {
+            println!("{}", err);
+            return;
+        }
+    };
 
     println!(
         "{} {} = {} {}",
         arguments.amount.purple().bold(),
         arguments.input.to_uppercase().red().bold(),
-        output.purple().bold(),
+        output.bold().purple(),
         arguments.output.to_uppercase().green().bold()
     );
 }
 
-fn convert(currency: String) -> String {
+fn convert(currency: String) -> Result<String, String> {
     let body: String =
         ureq::get(format!("https://api.exchangerate-api.com/v4/latest/{}", currency).as_str())
             .call()
-            .expect("Failed to call fn")
+            .map_err(|err| format!("{} {}", "Failed to call function".red().bold(), err))?
             .into_string()
-            .expect("failed to stringify");
-    body
+            .map_err(|err| format!("{} {}", "Failed to index Currency".red().bold(), err))?;
+    Ok(body)
 }
 
 // for example Currency * amount
-fn deserialize(input: String, currency: String, amount: i32) -> f64 {
-    let jcurrency: Jcurrency = serde_json::from_str(input.as_str()).expect("");
+fn deserialize(input: String, currency: String, amount: i32) -> Result<f64, String> {
+    let jcurrency: Jcurrency = match serde_json::from_str(input.as_str()) {
+        Ok(currency_out) => currency_out,
+        Err(_) => {
+            return Err(format!(
+                "{} {} {}",
+                "one or more Currencies are unknown:".bold().red(),
+                input.bold().yellow(),
+                "\nDoes it exist?".bold().red()
+            ))
+        }
+    };
 
-    let cout = jcurrency.rates[currency.as_str()]
-        .as_f64()
-        .expect("failed to floatify it");
+    let cout = jcurrency.rates[currency.as_str()].as_f64().ok_or_else(|| {
+        format!(
+            "{} {} {}",
+            "Unknown Currency:".bold().red(),
+            currency.bold().yellow(),
+            "\nHave you spelled it right?".bold().red(),
+        )
+    })?;
 
-    cout * amount as f64
+    Ok(cout * amount as f64)
 }
